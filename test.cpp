a44baf93b2649463cd39fd359a1ff705c6a84b57
willtirone <will.tirone1@gmail.com>
2021-05-05 19:38:40 -0400

    commiting for today

--- test.cpp
+++ test.cpp
@@ -5,17 +5,8 @@
 
 int main()
 {
-    int number_of_steps = 39; // int for integers 
-    double flying_time = 3.523412341234; // double for floating point numbers 
-    char decimal_point = '.'; // individual characters 
-    string name = "Annemarie"; // string for character strings
-    bool tap_on = true; // for logical variables
-    cout << "finished running\n\n"; 
-
-    cout <<"Please enter your first name and age\n";
-    string first_name; // string var 
-    int age; //integer var 
-    cin >> first_name; //read a string 
-    cin >> age; //read integer 
-    cout << "Hello," << first_name << " (age " << age << ")\n"; 
+    cout<<"Please enter a floating point value";
+    double n; 
+    cin >> n; 
+    cout << "n ==" << n; 
 }
