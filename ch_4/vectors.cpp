3557541fb0826fd2ef64c3eb4d7b26271828ffc1
willtirone <will.tirone1@gmail.com>
2021-05-25 17:19:30 -0400

    adding learning stuff

--- ch_4/vectors.cpp
+++ ch_4/vectors.cpp
@@ -3,8 +3,12 @@
 
 int main() 
 { 
-    std::vector<int> v(3);
+    std::vector<double> v;
 
     v.push_back(2.7);
     v.push_back(5.6); 
+    v.push_back(7.9); 
+
+    for(int i=0; i<v.size(); ++i)
+        std::cout << "v[" << i << "]" << v[i] << '\n';
 }
