fb6ce76bfc5ccbb66de74dfbc9864d85aef06fa4
willtirone <will.tirone1@gmail.com>
2021-10-15 11:27:30 -0400

    continuing on!

--- ch_5/range_errors.cpp
+++ ch_5/range_errors.cpp
@@ -2,10 +2,16 @@
 #include "/home/willtirone/learn_cpp/std_lib_facilities.h"
 
 int main()
-{
+try {
     vector <int> v; //a vector of ints
     int i; 
     while (std::cin>>i) v.push_back(i); //get values
-    for (int i=0;i<v.size(); ++i)
+    for (int i=0;i<=v.size(); ++i)
         std::cout<<"v[" << i << "]==" << v[i] << endl;
+} catch (out_of_range) {
+    cerr << "Oops! Range error \n";
+    return 1;
+} catch (...) {
+    cerr << "Exception : something went wrong \n"; 
+    return 2; 
 }
