ddad930a9f0ed3e72f3f90787896040071fd3289
willtirone <will.tirone1@gmail.com>
2021-11-10 16:55:54 -0500

    calculator from book

--- ch_6/calculator.cpp
+++ ch_6/calculator.cpp
@@ -1,17 +1,28 @@
 #include "../std_lib_facilities.h"
 
+class Token{
+public: 
+    char kind;
+    double value; 
+    Token(char ch) // make a Token from a character 
+        :kind(ch), value(0){}
+    Token(char ch, double val) // make a Token from a char and a double 
+        :kind(ch), value(val){} 
+}; 
+
+
 int main()
 {
     cout << "Please enter an expression (we can handle +,-,*, and /): "; 
     int lval = 1;
     int rval; 
     char op; 
-    cin >> lval;
+    cin >> lval; // left value 
     if (!cin)error("no first operand"); 
 
-    while (cin>>op){
-        cin>>rval;
-        if(!cin)error("no second operand");
+    while (cin>>op){ // read operator 
+        cin>>rval; // read right hand value repeatedly
+        if(!cin)error("no second operand"); 
         switch(op){
             case '+':  
                 lval += rval;
